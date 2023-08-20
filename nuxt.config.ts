f47d285d10db19dd6d3dec0275a499aed982180d
Bobbaru <bobbaru_krueger@yahoo.com>
2023-08-20 23:07:42 +0300

    add title and favicon

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,20 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  devtools: { enabled: true }
-})
+  devtools: { enabled: true },
+
+  app: {
+    head: {
+      title: "Nuxt Bundling",
+      link: [
+        // { rel: "dns-prefetch", href: `http://localhost:3000/` },
+        // { rel: "preconnect", href: `http://localhost:3000/` },
+        {
+          rel: "icon",
+          type: "image/png",
+          href: "/favicon.ico",
+        },
+      ],
+    },
+    pageTransition: { name: "page", mode: "out-in" },
+  },
+});
